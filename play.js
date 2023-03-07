9e61542ba109014387fc0eecd68ea7394b631782
jadallred <123498729+jadallred@users.noreply.github.com>
2023-03-06 20:34:17 -0700

    Update play.js

--- play.js
+++ play.js
@@ -91,3 +91,88 @@ class Game {
     this.allowPlayer = true;
   }
 
+  getPlayerName() {
+    return localStorage.getItem('userName') ?? 'Mystery player';
+  }
+
+  async playSequence() {
+    await delay(500);
+    for (const btn of this.sequence) {
+      await btn.press(1.0);
+      await delay(100);
+    }
+  }
+
+  addButton() {
+    const btn = this.getRandomButton();
+    this.sequence.push(btn);
+  }
+
+  updateScore(score) {
+    const scoreEl = document.querySelector('#score');
+    scoreEl.textContent = score;
+  }
+
+  async buttonDance(laps = 1) {
+    for (let step = 0; step < laps; step++) {
+      for (const btn of this.buttons.values()) {
+        await btn.press(0.0);
+      }
+    }
+  }
+
+  getRandomButton() {
+    let buttons = Array.from(this.buttons.values());
+    return buttons[Math.floor(Math.random() * this.buttons.size)];
+  }
+
+  saveScore(score) {
+    const userName = this.getPlayerName();
+    let scores = [];
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+      scores = JSON.parse(scoresText);
+    }
+    scores = this.updateScores(userName, score, scores);
+
+    localStorage.setItem('scores', JSON.stringify(scores));
+  }
+
+  updateScores(userName, score, scores) {
+    const date = new Date().toLocaleDateString();
+    const newScore = { name: userName, score: score, date: date };
+
+    let found = false;
+    for (const [i, prevScore] of scores.entries()) {
+      if (score > prevScore.score) {
+        scores.splice(i, 0, newScore);
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      scores.push(newScore);
+    }
+
+    if (scores.length > 10) {
+      scores.length = 10;
+    }
+
+    return scores;
+  }
+}
+
+const game = new Game();
+
+function delay(milliseconds) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, milliseconds);
+  });
+}
+
+function loadSound(filename) {
+  return new Audio('assets/' + filename);
+}
